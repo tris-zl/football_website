e0e6f52439ea6e67fde29a5771e754a18b036896
Tristan <tristanzell2006@gmail.com>
2022-11-25 16:24:28 +0100

    Button functions

--- Website/my_script.js
+++ Website/my_script.js
@@ -1,8 +1,25 @@
 
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
+document.getElementById("btn_1").onclick = function() {
+    var state = document.getElementById("myForm")
+    if(state.style.display == "block"){
+        state.style.display = "none"
+    }
+    else{
+        state.style.display = "block"
+    }
 }
 
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
+
+document.getElementById("menu_aufstellungen_btn").onclick = function() {
+    var state = document.getElementById("menu_links")
+    if(state.style.display == "block"){
+        state.style.display = "none"
+    }
+    else{
+        state.style.display = "block"
+    }
+}
+
